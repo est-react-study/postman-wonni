df317c723690a4f3df93c43892de2e968bf3634e
lsw8168 <lsw8168@naver.com>
2020-08-14 10:27:54 +0900

    style 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+import styles from "./css/scss.module.scss";
+
 const App: React.FC = () => {
-  return <div>postman wonni!!</div>;
+  return (
+    <div>
+      <div>postman wonni!!</div>
+      <div className={`${styles.icon} ${styles.icon_red}`} />
+      <div className={`${styles.icon} ${styles.icon_orange}`} />
+      <div className={`${styles.icon} ${styles.icon_yellow}`} />
+      <div className={`${styles.icon} ${styles.icon_green}`} />
+      <div className={`${styles.icon} ${styles.icon_blue}`} />
+      <div className={`${styles.icon} ${styles.icon_indigo}`} />
+      <div className={`${styles.icon} ${styles.icon_violet}`} />
+    </div>
+  );
 };
 
 export default App;
